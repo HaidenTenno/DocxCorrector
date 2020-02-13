0f2dd5196701263f21de55e508eb81ff882c9ed1
Petr Tartynskikh <haiden_tenno@mail.ru>
2020-02-14 01:01:28 +0300

    Start of dev

---
+++ DocxCorrector/DocxCorrector/App/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Resources;
+using DocxCorrector.Services;
+using Word = Microsoft.Office.Interop.Word;
+
+
+namespace DocxCorrector
+{
+    class Program
+    {
+        public static Corrector corrector = new CorrectorImpementation();
+
+        static void Main(string[] args)
+        {
+            // TODO: Remove
+            corrector.SayHi();
+
+            string filePath = @"C:\Users\haide\Desktop\docxcorrector_sharp\DocxCorrector\DocxCorrector\DocxFiles\Doc1.docx";
+
+            // Interop.Word version
+
+            Word.Application app = new Word.Application();
+            Word.Document document = app.Documents.Open(filePath);
+
+            foreach (Word.Paragraph paragraph in document.Paragraphs)
+            {
+                Console.WriteLine(paragraph.Range.Text);
+            }
+
+            document.Close();
+
+            // GemBox.Document version
+
+        }
+                
+    }
+}
---
+++ DocxCorrector/DocxCorrector/Services/Corrector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DocxCorrector.Services
+{
+    public abstract class Corrector
+    {
+        // TODO: Remove
+        public abstract void SayHi();
+    }
+
+    public sealed class CorrectorImpementation: Corrector
+    {
+        // TODO: Remove
+        public override void SayHi() => Console.WriteLine("Hi, Corrector");
+    }
+}
