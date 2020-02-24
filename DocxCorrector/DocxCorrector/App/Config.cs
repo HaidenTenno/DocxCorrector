498612355f2fb7e28088a57671db2f182d5f7e54
Митя Пономарёв <decolte070@gmail.com>
2020-02-24 23:28:02 +0300

    Зашёл сюда в первый раз

--- DocxCorrector/DocxCorrector/App/Config.cs
+++ DocxCorrector/DocxCorrector/App/Config.cs
@@ -4,12 +4,12 @@ namespace DocxCorrector.App
     public static class Config
     {
         // Docx файл для проверки
-        public const string DocFilePath = @"C:\Users\haide\Desktop\testDoc.docx";
+        public const string DocFilePath = @"C:\Users\Дмитрий\Desktop\paragraph_check\paragraph_check.docx";
         // Файл для записи ошибок
         public const string MistakesFilePath = @"C:\Users\haide\Desktop\mistakes.json";
         // Файл для записи свойств параграфов
         public const string PropertiesFilePath = @"C:\Users\haide\Desktop\properties.csv";
         // Корневая директория с файлами, из которых нужно вытянуть свойства
-        public const string FilesToInpectDirectoryPath = @"C:\Users\haide\Desktop\FilesToInspect";
+        public const string FilesToInpectDirectoryPath = @"C:\Users\Дмитрий\Desktop\paragraph_check\FilesToInspect";
     }
 }
